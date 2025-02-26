f3188ca674ec8d5e4cb3a7ddec1fdd5546643fc7
Harry Towers <107466346+kcsharry@users.noreply.github.com>
2025-02-26 10:58:39 +0000

    Set up GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  base: '/', // Keep as '/' since it's a user site
+  plugins: [react()],
 });
